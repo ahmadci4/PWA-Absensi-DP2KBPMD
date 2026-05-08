e2f42745a4ff10e7bc4ec5f65fba94e27c7fdc4e
ahmadci4 <114052872+ahmadci4@users.noreply.github.com>
2026-05-08 16:17:20 +0800

    Add files via upload

---
+++ js/drive.js
@@ -0,0 +1,56 @@
+// js/drive.js
+
+// Database Link Google Drive berdasarkan Nama SPMD/PLKB Bulan Mei 2026
+export const driveLinks = {
+    "SAKA PARAMUDITA, S.Kom": "https://drive.google.com/drive/folders/1/XIGAwCcE-aQrfug2r2iQFsCLY3FSHIK?usp=drive_link",
+    "HANIATI, S.AP": "https://drive.google.com/drive/folders/18mMuCwPmMoHn9CHdOjVyDITSjxHfboJ?usp=drive_link",
+    "TITIP ANDRIANI, S.Ikom": "https://drive.google.com/drive/folders/1zJZCPpOnYDvw8bqu6D105Dn3Vlbl5n80?usp=drive_link",
+    "HERMAN YADI, S.H": "https://drive.google.com/drive/folders/1irQrJIERXZVW2z49leMVgG51IVCMI6u9?usp=drive_link",
+    "TITIN OKTARIANTI, S.AK": "https://drive.google.com/drive/folders/1TitD-jOeUt5ePEdyUzURjYWbOKXRCtn?usp=drive_link",
+    "AHMAD TAUFIK, S.Pdi": "https://drive.google.com/drive/folders/1/WOcnS18QP6JwqsXW1Z9madPdiXaONzm?usp=drive_link",
+    "DESY YULIANA APRILIA, SP": "https://drive.google.com/drive/folders/1VLUq61tW25EgYRipDoGvPWcpusLPRQ-?usp=drive_link",
+    "HIDAYATUL SULUK, S.Hi": "https://drive.google.com/drive/folders/1aQGGeIDRGXMCDNyoZwK7Q5YhxpjsG63?usp=drive_link",
+    "MUSTAIN, S.Sos.I": "https://drive.google.com/drive/folders/1R6NrtxTSArEVp19plEP50Nfgb4GEjgYi?usp=drive_link",
+    "YULISDA AINUN, S.Sos": "https://drive.google.com/drive/folders/1yVq9eEefDYFcdmQhV71N9oSNADcFkiK7?usp=drive_link",
+    "PADMAWADI, S.Pd.": "https://drive.google.com/drive/folders/1zMXsF-b6WgedCeGwkLtDJU8qXyhCkE8Q?usp=sharing",
+    "AGUS SUPARNO, S.Hi": "https://drive.google.com/drive/folders/1/Uw9jMsJ6MMNFIhKxTjh1Wl6vfQsQbwu?usp=drive_link",
+    "AHMAD BADARUDIN, S.AP": "https://drive.google.com/drive/folders/10TiunsQowEXBaYeob7skoYVqz0SmX66X?usp=drive_link",
+    "ZAZLI RAIS, S.Pd": "https://drive.google.com/drive/folders/1YrgJR7cvDx1P6x5HndsDShlOVm1tOPH?usp=drive_link",
+    "BAIQ VIVI ALAYDIA, S.P": "https://drive.google.com/drive/folders/12fM4W119yHASXWQbO8PxNOvpE-lafYxD?usp=sharing",
+    "FIDIAN MUJIHADI, S.Pdi": "https://drive.google.com/drive/folders/1E7rr19pVqqFcxMtz6RLMYt4AxQJqR2Mf?usp=drive_link",
+    "DITA RUSDIANA, SP.": "https://drive.google.com/drive/folders/1vINCMzit3tNlMk8R1Fzh0jVGQG-uMOqj?usp=drive_link",
+    "JUMRATUL AINI, S.M": "https://drive.google.com/drive/folders/1060SwgTcNZdvHjwPECHqTHeRofnga0n7?usp=drive_link",
+    "MUHAMAD JEPRI AGUNG SUPRIANTO, S.Pd": "https://drive.google.com/drive/folders/1fdcj1Rym48Q4RuO0VARdCyiqKrZpn/WL?usp=drive_link",
+    "DEDI IRAWANTO, S.Sos": "https://drive.google.com/drive/folders/1TyprLXXb7110UOos1HTGNLJ-S20VWEj?usp=drive_link",
+    "LALU RIAN, S.E": "https://drive.google.com/drive/folders/1K79gnSHoVli63rgY1D3abuJTTKbkAFT?usp=drive_link",
+    "MALAYANTI, S.Kom": "https://drive.google.com/drive/folders/1YuenliJGpv1pf6NJkgrlUhClEgTNXJCr?usp=drive_link",
+    "HAIRI IFAN YURDANI, S.Pd": "https://drive.google.com/drive/folders/11FLOTX8LI6G48xG16TDkFm2xw-v2QJMB?usp=drive_link",
+    "ZULKIPLI ALI, S,Pd": "https://drive.google.com/drive/folders/1qv3KFL6b0Zm1KoZiuiZLSGhV6J060p?usp=sharing",
+    "AGUS SETIAWAN PUTRA JATI, AdM. Par": "https://drive.google.com/drive/folders/1UdHVHaJuBJ2k06nbmNpE5agbsyoUqkl?usp=drive_link",
+    "DESI ATMA BAKTI, S.Kh": "https://drive.google.com/drive/folders/17QKBqq0XeTicg8Dnm6A6P01fbLvslj?usp=sharing",
+    "HADIYANTO, S.Pd": "https://drive.google.com/drive/folders/1kJleYCC4SEXIZD8HPfUxXdTbm1ldO?usp=drive_link",
+    "BAIQ ERNA SUPIANTI, S.Pd": "https://drive.google.com/drive/folders/1/rRrMlupCK1F8cTcnTAOKj5uFr9WjwiW?usp=drive_link",
+    "ORI SAPTA VIANSYAH, S.Pd": "https://drive.google.com/drive/folders/1NXyJ4-t1wrR-os9Hk-OBQ-1GsiOifjx?usp=sharing",
+    "ZULKIPLI, S.P": "https://drive.google.com/drive/folders/1pFQRWXYCredA2xWF2N2qE7RxD9mXnYS?usp=sharing",
+    "HERMAN ZUHDI, ST": "https://drive.google.com/drive/folders/1NRzcYbXtKB4mkK9HkjKiagTYOJXcNP?usp=drive_link",
+    "SATRIADI, S.Pd": "https://drive.google.com/drive/folders/1Q0UdbUVoaHG4dCnk9ih47el8ez8x17rf?usp=drive_link",
+    "DENDA SISKA ROSANTI, S.Hub.In": "https://drive.google.com/drive/folders/1c4AggCf8zVtOG-Sf9K9X5kTuH9hSHY2R?usp=drive_link",
+    "AHMAD YANI, S.Kom": "https://drive.google.com/drive/folders/15z6ZqxrUHTrI0VQ557usAMgPBWK59Tww?usp=drive_link",
+    "SAPRUN, S.Sos": "https://drive.google.com/drive/folders/1f1KTDpq3z6JYD3OgZeceOvVCZJdQuDDF?usp=drive_link"
+};
+
+// Fungsi pintar untuk mencari link berdasarkan nama
+export function getDriveLinkByName(name) {
+    // Cari nama yang paling cocok (mengabaikan huruf besar/kecil atau salah spasi sedikit)
+    const normalizedTargetName = name.trim().toUpperCase();
+    
+    for (const [dbName, link] of Object.entries(driveLinks)) {
+        // Jika nama di aplikasi mengandung kata kunci dari nama di PDF atau sebaliknya
+        if (dbName.toUpperCase().includes(normalizedTargetName) || normalizedTargetName.includes(dbName.toUpperCase().split(',')[0])) {
+            return link;
+        }
+    }
+    
+    // Jika tidak ditemukan, kembalikan null
+    return null;
+}
